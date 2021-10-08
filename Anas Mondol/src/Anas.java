003b8279b828f9a4aee50dd3bba2dbac43e3a370
Md. Anas Mondol <62255256+Anas436@users.noreply.github.com>
2021-10-08 23:01:40 +0600

    Java

---
+++ Anas Mondol/src/Anas.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+public class Anas {
+
+    
+    public static void main(String[] args) {
+        
+        Scann
+       
+        float a = 3.09f;
+       double number =10;
+       int c =18;
+       
+       byte d= (byte) a;
+       double b= (double) c;
+       int e = (int) number;
+       
+       System.out.println(d);
+       System.out.println(b);
+       System.out.println(e);
+       
+      
+       
+   
+       
+       
+    }
+    
+}
